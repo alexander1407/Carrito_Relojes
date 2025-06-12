93d3cdac0d7d846615c51aa6aeeb0bdc9ad12752
Alexander007 <menavivanconoe@outlook.es>
2025-06-12 15:38:29 -0500

    Estructura base del script y evento DOMContentLoaded

---
+++ app.js
@@ -0,0 +1,13 @@
+// Variable que mantiene el estado visible del carrito
+var carritoVisible = false;
+
+// Esperamos que todos los elementos de la página carguen para ejecutar el script
+if (document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready();
+}
+
+function ready() {
+    // Aquí después iremos agregando la funcionalidad de los botones
+}
